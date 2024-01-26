f61c2bbf543726da0fed0388f3bb1ed5f6b8e7f3
mhalonso96 <celo.alonso@gmail.com>
2024-01-26 15:16:48 -0300

    ♻️ refresh code

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -1,19 +1,23 @@
 const Header = (props) => {
+console.log(props)
 return (
-  <h1>{props.course}</h1>
+  <h1>{props.course.name}</h1>
 )
 }
+
 const Content = (props) => {
-  
+    console.log(props)
   return (
     <div>
-      <Part parts={props.parts[0]} exercises={props.exercises[0]}/>
-      <Part parts={props.parts[1]} exercises={props.exercises[1]}/>
-      <Part parts={props.parts[2]} exercises={props.exercises[2]}/>
+      <Part parts={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part parts={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part parts={props.parts[2].name} exercises={props.parts[2].exercises}/>
     </div>
   )
 }
+
 const Part = (props) => {
+  console.log(props)
   return(
     <p>
         {props.parts} {props.exercises}
@@ -21,24 +25,34 @@ const Part = (props) => {
   )
 }
 const Total = (props) => {
+  console.log(props)
   return (
-    <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 const App = () => {
-  const course = 'Desenvolvimento de aplicação Half Stack'
-  const part1 = 'Fundamentos da biblioteca React'
-  const exercises1 = 10
-  const part2 = 'Usando props para passar dados'
-  const exercises2 = 7
-  const part3 = 'Estado de um componente'
-  const exercises3 = 14
-
+   const course = {
+    name: 'Desenvolvimento de aplicação Half Stack',
+    parts: [
+      {
+        name: 'Fundamentos da biblioteca React',
+        exercises: 10
+      },
+      {
+        name: 'Usando props para passar dados',
+        exercises: 7
+      },
+      {
+        name: 'Estado de um componente',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
       <Header course={course} />
-      <Content parts= {[part1,part2,part3]} exercises ={[exercises1,exercises2,exercises3]}/>
-      <Total exercises = {[exercises1,exercises2,exercises3]}/>
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
---
+++ teste.js
@@ -0,0 +1,6 @@
+const media = function(a, b) { // *média
+    return (a + b) / 2
+  }
+  
+  const resultado = media(2, 5)
+  console.log(resultado)
