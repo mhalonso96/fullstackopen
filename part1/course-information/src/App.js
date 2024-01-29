e75582570a1916329294147fa338dd1007038174
mhalonso96 <celo.alonso@gmail.com>
2024-01-29 11:14:58 -0300

    ✅ commit exercises part1 - course informaton

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -42,103 +42,128 @@ const Botao = ({ onClick, texto }) => (
     {texto}
   </button>
 )
-// const App = () => {
-//    const course = {
-//     name: 'Desenvolvimento de aplicação Half Stack',
-//     parts: [
-//       {
-//         name: 'Fundamentos da biblioteca React',
-//         exercises: 10
-//       },
-//       {
-//         name: 'Usando props para passar dados',
-//         exercises: 7
-//       },
-//       {
-//         name: 'Estado de um componente',
-//         exercises: 14
-//       }
-//     ]
-//   }
-//   const [contador, setContador] = useState(0)
-//   // setTimeout(
-//   //   () => setContador(contador + 1), 1000
-//   // )
-//   console.log('renderizando...', contador)
-//   const aumentarEmUm = () => setContador(contador + 1)
-//   const diminuirEmUm = () => setContador(contador - 1)
-//   const zerarContador = () => setContador(0)
-
-//   const [esquerda, setEsquerda] = useState(0) 
-//   const [direita, setDireita] = useState(0) 
-
-//   return (
-//     <div>
-//       <Header course={course} />
-//       <Content parts={course.parts} />
-//       <Total parts={course.parts} />
-//       <div>
-//         <Exibir contador={contador}/>
-//         <Botao
-//         onClick={aumentarEmUm}
-//         texto='mais+'
-//       />
-//       <Botao
-//         onClick={zerarContador}
-//         texto='zerar'
-//       />     
-//       <Botao
-//         onClick={diminuirEmUm}
-//         texto='menos-'
-//       />
-//       </div>
-//       <div>
-//       {esquerda}
-//       <button onClick={() => setEsquerda(esquerda + 1)}>
-//         Esquerda
-//       </button>
-//       <button onClick={() => setDireita(direita + 1)}>
-//         Direita
-//       </button>
-//       {direita}
-//     </div>
-      
-//     </div>
-    
-//   )
-// }
 const App = () => {
-  const [esquerda, setEsquerda] = useState(0)
-  const [direita, setDireita] = useState(0)
-
-  const [todosOsCliques, setTodos] = useState([])
-  const [total, setTotal] = useState(0)
-
-
-  const handleCliqueEsquerda = () => {
-    setTodos(todosOsCliques.concat('E'))
-    const atualizaEsquerda = esquerda + 1
-    setEsquerda(atualizaEsquerda)
-    setTotal(atualizaEsquerda + direita)
-  } 
-
-
-  const handleCliqueDireita = () => {
-    setTodos(todosOsCliques.concat('D'))
-    const atualizaDireita = direita + 1
-    setDireita(atualizaDireita)
-    setTotal(esquerda + atualizaDireita)
+   const course = {
+    name: 'Desenvolvimento de aplicação Half Stack',
+    parts: [
+      {
+        name: 'Fundamentos da biblioteca React',
+        exercises: 10
+      },
+      {
+        name: 'Usando props para passar dados',
+        exercises: 7
+      },
+      {
+        name: 'Estado de um componente',
+        exercises: 14
+      }
+    ]
   }
+  const [contador, setContador] = useState(0)
+  // setTimeout(
+  //   () => setContador(contador + 1), 1000
+  // )
+  console.log('renderizando...', contador)
+  const aumentarEmUm = () => setContador(contador + 1)
+  const diminuirEmUm = () => setContador(contador - 1)
+  const zerarContador = () => setContador(0)
+
+  const [esquerda, setEsquerda] = useState(0) 
+  const [direita, setDireita] = useState(0) 
 
   return (
     <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+      <div>
+        <Exibir contador={contador}/>
+        <Botao
+        onClick={aumentarEmUm}
+        texto='mais+'
+      />
+      <Botao
+        onClick={zerarContador}
+        texto='zerar'
+      />     
+      <Botao
+        onClick={diminuirEmUm}
+        texto='menos-'
+      />
+      </div>
+      <div>
       {esquerda}
-      <button onClick={handleCliqueEsquerda}>Esquerda</button>
-      <button onClick={handleCliqueDireita}>Direita</button>
+      <button onClick={() => setEsquerda(esquerda + 1)}>
+        Esquerda
+      </button>
+      <button onClick={() => setDireita(direita + 1)}>
+        Direita
+      </button>
       {direita}
-      <p>{todosOsCliques.join(' ')}</p>
-      <p>Total {total}</p>
     </div>
+      
+    </div>
+    
   )
 }
 export default App
+// TREINAMENTO TUTORIAL COURSE - 1
+
+// const Historico = (props) => {
+//   if (props.todosOsCliques.length == 0) {
+//     return (
+//       <div>
+//         Clique em um dos botões para usar a aplicação!
+//       </div>
+//     )
+//   }
+//   return (
+//     <div>
+//       Histórico de cliques nos botões: {props.todosOsCliques.join(' ')}
+//     </div>
+//   )
+// }
+// const App = () => {
+//   const [esquerda, setEsquerda] = useState(0)
+//   const [direita, setDireita] = useState(0)
+//   const [todosOsCliques, setTodos] = useState([])
+//   const [total, setTotal] = useState(0)
+
+//   const handleCliqueEsquerda = () => {
+//     setTodos(todosOsCliques.concat('E'))
+//     console.log('clique esquerdo anterior', esquerda)
+//     const atualizaEsquerda = esquerda + 1
+//     setEsquerda(atualizaEsquerda)
+//     setTotal(atualizaEsquerda + direita)
+//   } 
+
+//   const handleCliqueDireita = () => {
+//     setTodos(todosOsCliques.concat('D'))
+//     console.log('clique direita anterior', direita)
+//     const atualizaDireita = direita + 1
+//     setDireita(atualizaDireita)
+//     setTotal(atualizaDireita + esquerda)
+//   }
+//   const handleCliqueReset = () => {
+//     setDireita(0)
+//     setEsquerda(0)
+//     setTodos([])
+//     setTotal([])
+
+//   }
+
+//   return (
+//     <div>
+//       {esquerda}
+//       <Botao onClick={handleCliqueEsquerda} texto='Esquerda' />
+//       <Botao onClick={handleCliqueDireita} texto='Direita' />
+//       {direita}
+//       <Botao onClick={handleCliqueReset} texto='Reset' />
+//       <Historico todosOsCliques={todosOsCliques} />
+      
+//     </div>
+//   )
+// }
+// export default App
+
